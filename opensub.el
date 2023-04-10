32204e5acb943e17698c5ec52dc6b32d8d44d232
Daniel Fleischer <danflscr@gmail.com>
2023-04-10 20:07:38 +0300

    Initial commit

---
+++ opensub.el
@@ -0,0 +1,34 @@
+;;; opensub.el --- search and download from open-subtitles  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Daniel Fleischer
+
+;; Author: Daniel Fleischer <danflscr@gmail.com>
+;; Keywords: multimedia
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+
+
+(defun opensub ()
+  (interactive)
+  (message "This is all, folks."))
+
+(provide 'opensub)
+;;; opensub.el ends here
