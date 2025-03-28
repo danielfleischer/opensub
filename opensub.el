3989e46a524f0e47b88b60c50a1cbb1d0e42cf5a
Daniel Fleischer <danflscr@gmail.com>
2025-03-29 00:05:12 +0300

    Replacing all url code with plz

--- opensub.el
+++ opensub.el
@@ -6,7 +6,7 @@
 ;; Keywords: multimedia
 ;; Homepage: https://github.com/danielfleischer/opensub
 ;; Version: 1.0.0
-;; Package-Requires: ((emacs "25.1"))
+;; Package-Requires: ((emacs "25.1") (plz))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -36,6 +36,8 @@
 (require 'cl-lib)
 (require 'json)
 (require 'webjump)
+(require 'plz)
+
 
 (defgroup opensub nil
   "Search and fetch subtitles from opensubtitles.com."
@@ -57,40 +59,26 @@
   "Width of the column in the completion list."
   :type 'integer)
 
-(defvar opensub--json-fn
-  (if (version<= "27.1" emacs-version)
-      (lambda () (json-parse-buffer :object-type 'alist))
-    #'json-read)
-  "JSON parsing function.")
-
-(defun opensub--curl (url)
-  "Given a URL, return parsed json."
-  (with-current-buffer
-      (url-retrieve-synchronously url)
-    (goto-char (point-min))
-    (search-forward "\n\n")
-    (delete-region (point-min) (point))
-    (funcall opensub--json-fn)))
-
 (defun opensub--curl-retrieve (query)
   "Run a QUERY on opensubtitles.com. Return parsed json."
   (let* ((clean-query (webjump-url-encode query))
-         (url-request-method "GET")
-         (url-request-extra-headers
-          `(("Api-Key" . ,opensub-api-key)
-            ("Content-Type" . "application/json")))
          (url (format (concat "https://api.opensubtitles.com/api/v1/subtitles?query=%s"
                               (format "&languages=%s"
                                       (string-join (sort opensub-languages #'string<) ",")))
                       clean-query)))
-    (opensub--curl url)))
+    (plz 'get url
+      :headers `(("Content-Type" . "application/json")
+                 ("Accept" . "application/json")
+                 ("Api-key" . ,opensub-api-key))
+      :as #'json-read)))
+
 
 (defun opensub--annotation (cand)
   (let* ((rest (cdr (assoc cand minibuffer-completion-table))))
     (concat "  "
-     (propertize (alist-get 'language rest) 'face 'success) "  "
-     (propertize (string-pad (alist-get 'rating rest) 4) 'face 'warning) "  "
-     (propertize (alist-get 'full_name rest)))))
+            (propertize (alist-get 'language rest) 'face 'success) "  "
+            (propertize (string-pad (alist-get 'rating rest) 4) 'face 'warning) "  "
+            (propertize (alist-get 'full_name rest)))))
 
 (defun opensub--get-file-id (results)
   "Given query RESULTS, return the file id of user-selected item."
@@ -112,20 +100,18 @@
                             items)))
     (alist-get 'file_id (aref (let-alist item .attributes.files) 0))))
 
-
 (defun opensub--curl-get-download-info (media-id)
   "Given a MEDIA-ID, generate a downloadable link.
 Returns alist with link and file name.
 Uses a POST call to generate a time-limited link.
 May fail if exceeds daily usage limits."
-  (let* ((url-request-method "POST")
-         (url-request-extra-headers
-          `(("Api-Key" . ,opensub-api-key)
-            ("Content-Type" . "application/json")))
-         (url-request-data
-          (format "{\"file_id\": %s}" media-id))
-         (response
-          (opensub--curl "https://api.opensubtitles.com/api/v1/download")))
+  (let ((response
+         (plz 'post "https://api.opensubtitles.com/api/v1/download"
+           :headers `(("Content-Type" . "application/json")
+                      ("Accept" . "application/json")
+                      ("Api-key" . ,opensub-api-key))
+           :body (json-encode `(("file_id" . ,media-id)))
+           :as #'json-read)))
     (if (alist-get 'link response) response
       (user-error (alist-get 'message response)))))
 
